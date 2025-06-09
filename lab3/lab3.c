9700c3423fae84c248f4d7b2f6a2f999cef1db9c
Harshit (Harry) Gupta <hgupta@sfu.ca>
2025-06-08 17:19:43 -0700

    lab3

---
+++ lab3/lab3.c
@@ -0,0 +1,57 @@
+// Lab3
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SIZE 5
+
+void print_old(char *old[], int count, int start) {
+  for (int i = 0; i < count; i++) {
+    int index = (start + i) % SIZE;
+    printf("%s", old[index]);
+  }
+}
+
+void free_old(char *old[], int count) {
+  for (int i = 0; i < count; i++) {
+    free(old[i]);
+  }
+}
+
+int main() {
+  char *old[SIZE] = {NULL};
+  int count = 0;
+  int start = 0;
+
+  char *line = NULL;
+  size_t len = 0;
+
+  while (1) {
+    printf("Enter input: ");
+    ssize_t readInput = getline(&line, &len, stdin);
+    if (readInput == -1) {
+      break;
+    }
+
+    if (strcmp(line, "print\n") == 0) {
+      print_old(old, count, start);
+      continue;
+    }
+
+    // Store new input
+    if (count < SIZE) {
+      old[(start + count) % SIZE] = strdup(line);
+      count++;
+    } else {
+      // overwrite oldest
+      free(old[start]);
+      old[start] = strdup(line);
+      start = (start + 1) % SIZE;
+    }
+  }
+
+  free(line);
+  free_old(old, count);
+  return 0;
+}
